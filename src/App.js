cddab24278b9b65787c33cd4625bce79de620740
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2022-11-07 14:19:28 +0200

    documentation changes

--- src/App.js
+++ src/App.js
@@ -53,7 +53,10 @@ function App() {
   Fetched from TMDB API, hard coded to avoid useless fetching, genres will most likely not change too often*/
   const genres = [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]
 
-  //SETS THE THEME FOR THE WEBAPP, (CHANGE FONT ITS BAD)
+  //SETS THE THEME FOR THE WEBAPP
+
+    //FONT LINK: <link href = "https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@500&display=swap" rel = "stylesheet" >
+    
   const theme = createTheme({
   //primary -> yläpalkki
   palette: {
