57361fb59f1378f4e424f3691d2c89e09e8f78ea
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2022-12-04 21:36:44 +0200

    dotenv

--- src/cred/cred.js
+++ src/cred/cred.js
@@ -1,8 +1,8 @@
-
+require('dotenv').config()
 
 //hidden with gitignore during production
 function getCreds() {
-    var api_key = process.env.API_KEY;
+    var api_key = process.env.REACT_APP_API_KEY;
     return api_key
 }
 
--- src/mui/MyWatchedList.js
+++ src/mui/MyWatchedList.js
@@ -4,6 +4,7 @@ import { Outlet} from "react-router-dom";
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import ReviewsIcon from '@mui/icons-material/Reviews';
 import EditIcon from '@mui/icons-material/Edit';
+require('dotenv').config()
 
 
 //USERS PERSONAL WATCHLIST FOR MOVIES THEY HAVE ALREADY SEEN
